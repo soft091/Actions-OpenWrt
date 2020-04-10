aba08c02b6b527ddbfb55e323d2bbb2f9b53edca
soft091 <62936655+soft091@users.noreply.github.com>
2020-04-10 18:12:43 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,10 +6,9 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-#git clone https://github.com/Lienol/openwrt-package package
+sed -i 's/192.168.1.1/192.168.118.254/g' package/base-files/files/bin/config_generate
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
-git clone https://github.com/Lienol/openwrt-package package/openwrt-package
+# git clone https://github.com/Lienol/openwrt-package package/openwrt-package
 ./scripts/feeds clean
 ./scripts/feeds update -a
 ./scripts/feeds install -a
