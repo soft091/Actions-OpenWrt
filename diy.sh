f5e4d333df372efc93525a17981e771d3fe90d49
soft091 <62936655+soft091@users.noreply.github.com>
2020-04-10 18:34:00 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,6 +9,8 @@
 sed -i 's/192.168.1.1/192.168.118.254/g' package/base-files/files/bin/config_generate
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 # git clone https://github.com/Lienol/openwrt-package package/openwrt-package
+# Theme
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat package/luci-theme-opentomcat
 ./scripts/feeds clean
 ./scripts/feeds update -a
 ./scripts/feeds install -a
