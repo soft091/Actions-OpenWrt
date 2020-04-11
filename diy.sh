549308b8c3afb1b2873beaa2dab8da5be8ac4ce1
soft091 <62936655+soft091@users.noreply.github.com>
2020-04-11 16:27:03 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -12,24 +12,9 @@ git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app
 # Theme
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat package/luci-theme-opentomcat
 # SmartDNS
-WORKINGDIR="`pwd`/feeds/packages/net/smartdns"
-mkdir $WORKINGDIR -p
-rm $WORKINGDIR/* -fr
-wget https://github.com/pymumu/openwrt-smartdns/archive/master.zip -O $WORKINGDIR/master.zip
-unzip $WORKINGDIR/master.zip -d $WORKINGDIR
-mv $WORKINGDIR/openwrt-smartdns-master/* $WORKINGDIR/
-rmdir $WORKINGDIR/openwrt-smartdns-master
-rm $WORKINGDIR/master.zip
+git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns/
+git clone https://github.com/pymumu/openwrt-smartdns feeds/packages/net/smartdns/
 
-LUCIBRANCH="lede" #更换此变量
-WORKINGDIR="`pwd`/feeds/luci/applications/luci-app-smartdns"
-mkdir $WORKINGDIR -p
-rm $WORKINGDIR/* -fr
-wget https://github.com/pymumu/luci-app-smartdns/archive/${LUCIBRANCH}.zip -O $WORKINGDIR/${LUCIBRANCH}.zip
-unzip $WORKINGDIR/${LUCIBRANCH}.zip -d $WORKINGDIR
-mv $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}/* $WORKINGDIR/
-rmdir $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}
-rm $WORKINGDIR/${LUCIBRANCH}.zip
 ./scripts/feeds clean
 ./scripts/feeds update -a
 ./scripts/feeds install -a
